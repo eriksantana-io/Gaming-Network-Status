9225b87f4479e43cb8bc2c264cb2d262995760ed
Erik Santana <esantana@gmail.com>
2015-01-29 23:03:17 -0500

    Initial Commit

---
+++ Gaming Network Status/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Gaming Network Status
+//
+//  Created by Erik Santana on 1/29/15.
+//  Copyright (c) 2015 Erik Santana. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Gaming Network StatusTests/Gaming_Network_StatusTests.swift
@@ -0,0 +1,36 @@
+//
+//  Gaming_Network_StatusTests.swift
+//  Gaming Network StatusTests
+//
+//  Created by Erik Santana on 1/29/15.
+//  Copyright (c) 2015 Erik Santana. All rights reserved.
+//
+
+import Cocoa
+import XCTest
+
+class Gaming_Network_StatusTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
